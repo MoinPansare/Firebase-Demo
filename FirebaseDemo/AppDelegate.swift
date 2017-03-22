122eb0d510783dcb56b747ae2db3a380a8491e42
Moin <moin.pansare@qaagility.com>
2017-03-22 21:13:47 +0530

    Added Facebook SignIn and added user to Firebase project

--- FirebaseDemo/AppDelegate.swift
+++ FirebaseDemo/AppDelegate.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions);
+        
+        FIRApp.configure()
+        
         return true
     }
 
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+        
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- FirebaseDemo/ViewController.swift
+++ FirebaseDemo/ViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import Firebase
+import FirebaseAuth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FBSDKLoginButtonDelegate {
 
+    var facebookLoginButton : FBSDKLoginButton?;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        facebookLoginButton = FBSDKLoginButton();
+        facebookLoginButton?.frame = CGRect(x: 16, y: 30, width: self.view.frame.size.width - 32, height: 50);
+        facebookLoginButton?.delegate = self;
+        facebookLoginButton?.readPermissions = ["public_profile", "email"];
+        self.view.addSubview(facebookLoginButton!);
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("logged out of facebook");
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print("Error in loggin in from facebook");
+            return;
+        }
+        
+        print("Facebook Login Successful...");
+        
+        
+        FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"email,name"], httpMethod: "GET").start(completionHandler: {
+            (connection, result, error) in
+            if(error == nil)
+            {
+                print("result \(result)")
+                let info = result as! Dictionary<String,Any>;
+                print("Emial : \(info["email"])");
+                self.addUserToFirebase();
+            }
+            else
+            {
+                print("error \(error)")
+            }
+        })
+        
+        
+    }
+    
+    func addUserToFirebase(){
+        let accessToken : String = FBSDKAccessToken.current().tokenString!;
+        let credentials = FIRFacebookAuthProvider.credential(withAccessToken: accessToken);
+        FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
+            if error == nil{
+                print("Something went wrong in adding user Facebook User to firebase : \(error)")
+            }
+            print("Successgully added Facebook User to Firebase DataSet");
+        })
     }
 
     override func didReceiveMemoryWarning() {
